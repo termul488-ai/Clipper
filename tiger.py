fc8e32940b3f3efde31d017d347d8b483e059c88
termul488-ai <termul488@gmail.com>
2026-07-23 17:44:00 +0700

    Remove version and email print statements
    Removed version and email display from output.

--- tiger.py
+++ tiger.py
@@ -42,8 +42,6 @@ faded_text = fade.fire(logo)
 print(faded_text)
 while attemps < 100:
 	print(f"\r\033[97m╔{'═' * 55}╗\033[0m")
-    print(f"\r\033[100m\033[97m║\033[32m   v.1.0{' ' * 41}\033[97m║\033[0m")
-    print(f"\r\033[100m\033[97m║\033[32m   https://kunkaffa@gmail.com{' ' * 21}\033[97m║\033[0m")
     print(f"\r\033[97m╚{'═' * 55}╝\033[0m")
 
     
