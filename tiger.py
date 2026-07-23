9b8a4bfa6486c5958ecedf42861dd6e68338ebdc
termul488-ai <termul488@gmail.com>
2026-07-23 17:54:35 +0700

    Update tiger.py

--- tiger.py
+++ tiger.py
@@ -110,9 +110,8 @@ def initHeaders():
 def handleStatusCodes(status_code):
 	time.sleep(2)
 	global request_counter
-	print(f"\033[33m "+str(url)+"\033[38;5;1m runing\033[0m")
-	sys.stdout.write("\033[100m \033[40m\r%i Target\033[0m" % request_counter) 
-	print(f"\033[48;5;5m "+str(url)+"\033[38;5;7m runing\033[0m")
+	sys.stdout.write("\033[100m \033[40m\r%i Target\033[0m" % request_counter)
+	print ("\n"+msg + " after %i requests" % request_counter)
 	sys.stdout.flush()
 	if status_code == 429:
 			printMsg("You have been throttled")
